632f2ee965c4c2eaa0ae987661780df994e805d3
Lysander Mealy <lysander.mealy@gmail.com>
2020-06-09 08:51:01 -0400

    Commit

--- example/sip.py
+++ example/sip.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import json
 import tkinter as tk
 
-configfile = open("config.json", "r")
+configfile = open("example/config.json", "r")
 config = json.loads(configfile.read())
 
 tree = ET.parse(config["path"])
